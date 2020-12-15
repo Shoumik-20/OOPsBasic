0302dad5f3ab2369de6345aa4aae4b1c49cd10f6
Shoumik-20 <55137590+Shoumik-20@users.noreply.github.com>
2020-12-15 12:03:55 +0530

    Video 1 completed

---
+++ Concepts/Classes_and_Instances.py
@@ -0,0 +1,25 @@
+# Python Object Oriented
+
+class Employee:
+
+    def __init__(self, first, last, pay):
+        self.first = first     #need not be the same
+        self.last = last
+        self.pay = pay
+        self.email = first + '.' + last + '@company.com'
+
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
+emp_1 = Employee('Corey', 'Schafer', 50000 )
+emp_2 = Employee('Test', 'User', 60000)
+
+# print(emp_1)
+# print(emp_2)
+
+print(emp_1.email)
+print(emp_2.email)
+
+print(emp_1.fullname()) # calling method in instance
+print(emp_2.fullname())
+
+print(Employee.fullname(emp_1)) # calling method in class
